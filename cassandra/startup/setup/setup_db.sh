20c6be082bbb5e5620754caee0720dd0cb9427f1
Lukas <2150580@stud.hs-mannheim.de>
2022-06-19 01:19:17 +0200

    change data loading into setup_db.sh
    - load data with script
    - add views by startup

--- cassandra/startup/setup/setup_db.sh
+++ cassandra/startup/setup/setup_db.sh
@@ -1,8 +1,11 @@
-
 #!/bin/bash
+cqlsh cass1 -f /tmp/startup/setup/setup_db_1.cql
+cqlsh cass1 -f /tmp/startup/setup/setup_db_2.cql
 # NOTE : Quote it else use array to avoid problems #
 # <stdin>:1:Failed to import 20 rows: WriteTimeout - Error from server: code=1100 [Coordinator node timed out waiting for replica nodes' responses] message="Operation timed out - received only 0 responses." info={'consistency': 'ONE', 'required_responses': 1, 'received_responses': 0, 'write_type': 'UNLOGGED_BATCH'},  will retry later, attempt 1 of 5
 FILES="/tmp/startup/data/relations/*"
 for f in $FILES; do
     cqlsh cass1 -e "COPY twitter.user (user_id_x,follower_id,name,author,content,country,date_time,id,language,latitude,longitude,number_of_likes,number_of_shares,user_id_y) FROM '$f' WITH DELIMITER=',' AND  HEADER=TRUE;"
 done
+
+cqlsh cass1 -f /tmp/startup/setup/setup_views.cql
