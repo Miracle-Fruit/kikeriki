dc32a4e8b6b1643844036a151a286f99c7c1b0f2
Lukas <2150580@stud.hs-mannheim.de>
2022-06-19 21:14:23 +0200

    delte old one

--- cassandra/startup/queries/exercise3.py
+++
@@ -1,9 +0,0 @@
-from cassandra.cluster import Cluster
-
-cluster = Cluster(['172.20.0.6', '172.20.0.7', '172.20.0.8'])
-session = cluster.connect('twitter')
-
-
-rows = session.execute('SELECT content FROM tweets LIMIT 10')
-for row in rows:
-    print(row.content)
